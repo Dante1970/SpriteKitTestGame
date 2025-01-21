61a862342053e2a726b5e679328808a561bf89fc
Sergey Belousov <seryozha.belousov@gmail.com>
2025-01-21 18:43:33 +0200

    add and setup camera

--- Crossy Road/GameScene.swift
+++ Crossy Road/GameScene.swift
@@ -11,6 +11,7 @@ import GameplayKit
 class GameScene: SKScene {
     
     private var player: Player!
+    private let cameraNode: SKCameraNode = SKCameraNode()
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor(red: 63 / 255, green: 111 / 255, blue: 24 / 255, alpha: 1)
@@ -19,6 +20,10 @@ class GameScene: SKScene {
         
         player = Player.populate(at: CGPoint(x: screen.size.width / 2, y: 300))
         addChild(player)
+        
+        addChild(cameraNode)
+        camera = cameraNode
+        cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
         
         addSwipeGestureRecognizers()
     }
@@ -35,6 +40,34 @@ class GameScene: SKScene {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         player.jump(isForward: true)
+        let visibleBounds = getVisibleBounds()
+        let playerOffsetThreshold = visibleBounds.minY + 300
+        
+        if player.position.y >= playerOffsetThreshold {
+            let targetCameraY = cameraNode.position.y + (player.position.y - playerOffsetThreshold)
+            let moveAction = SKAction.moveTo(y: targetCameraY, duration: 0.3)
+            moveAction.timingMode = .easeInEaseOut
+            cameraNode.run(moveAction)
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        cameraNode.position.y += 0.5
+    }
+    
+    private func getVisibleBounds() -> CGRect {
+        guard let view = self.view else { return .zero }
+
+        let width = view.bounds.width
+        let height = view.bounds.height
+
+        let centerX = cameraNode.position.x
+        let centerY = cameraNode.position.y
+
+        let minX = centerX - width / 2
+        let minY = centerY - height / 2
+
+        return CGRect(x: minX, y: minY, width: width, height: height)
     }
     
 }
