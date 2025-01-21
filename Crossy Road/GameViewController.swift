dc2a9c513c3268aa39360a08684b26a06bb43492
Sergey Belousov <seryozha.belousov@gmail.com>
2025-01-21 13:27:58 +0200

    сlean up code and delete redundant files

--- Crossy Road/GameViewController.swift
+++ Crossy Road/GameViewController.swift
@@ -15,12 +15,9 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
+            
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
                 view.presentScene(scene)
             }
             
@@ -32,11 +29,7 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
